fa7da03c0fea79476db18bf24e9cbf3b5165d10f
Lucas Siqueira <lucas.s.siqueira.2009@gmail.com>
2025-09-13 18:13:06 -0300

    Começo do "Funcionamento"

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+// Rodar quando o documento inicializa
+let opcoes = document.getElementsByClassName("option");
+let opcoesResp = document.getElementsByClassName("optionResp");
+opcoes[0].classList.toggle('selected');
+opcoesResp[0].classList.toggle('selected');
+
 // Menu responsivo
 let menuBtn = document.getElementById('menu');
 let menu = document.getElementById('menuResp');
@@ -7,3 +13,22 @@ let aberto = false;
 menuBtn.addEventListener('click', function () {
     navMob.classList.toggle('ativo');
 });
+
+// Funcionamento
+function opt(clicado, tipo) {
+
+    if (tipo == 'normal') {
+        for(var x = 0; x < opcoes.length; x++) {
+            opcoes[x].classList.remove('selected');
+        }
+    
+        opcoes[clicado].classList.toggle('selected');
+    } else if (tipo =='mobile') {
+        for(var x = 0; x < opcoesResp.length; x++) {
+            opcoesResp[x].classList.remove('selected');
+        }
+    
+        opcoesResp[clicado].classList.toggle('selected');
+    }
+
+}
